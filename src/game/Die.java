43561a0e9f3c03e46668fb01bfbbb5bf1bd9f2eb
Azeliah <s205460@student.dtu.dk>
2020-10-27 19:58:41 +0100

    Made changes to Die, so that you can choose the number of faces, and updated the instantiations of Die here and there. Also removed a redundant return in Die.

--- src/game/Die.java
+++ src/game/Die.java
@@ -2,13 +2,18 @@ package game;
 import java.lang.Math;
 
 public class Die {
+    // Variables
+    private int faceValue;
+    final private int dieMax;
 
-    int faceValue;
+    // Constructor, setting the highest face value of the dice, e.g. a D6 or D20.
+    public Die (int maxValue) {
+        dieMax = maxValue;
+    }
 
     //Roll the dice, and return the value
-    public int roll(){
-        this.faceValue = (int)(Math.random()*6) + 1;
-        return this.faceValue;
+    public void roll(){
+        this.faceValue = (int)(Math.random()*dieMax) + 1;
     }
 
     //Get the current value of the dice
