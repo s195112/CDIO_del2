952ca77b76dd37c39980f0140d4db7d6d689fd45
Bastian <basbafala@gmail.com>
2020-10-28 11:38:11 +0100

    Test dice spread

--- src/test/DieTest.java
+++ src/test/DieTest.java
@@ -4,15 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DieTest {
 
-    Die die = new Die(6);
+    int n = 6;
+    Die die = new Die(n);
+    int tests = 1000;
+
     @org.junit.jupiter.api.Test
     void roll() {
 
-        for (int i = 0; i < 1001; i++) {
+        for (int i = 0; i < tests; i++) {
 
             die.roll();
             int result = die.getFaceValue();
-            assertTrue(result>=1 && result<=6);
+            assertTrue(result>=1 && result<=n);
         }
+
+        int[] spread = new int[n];
+
+        for (int i = 0; i < tests; i++) {
+            die.roll();
+            int result = die.getFaceValue();
+            spread[result-1]++;
+
+        }
+
     }
 }
