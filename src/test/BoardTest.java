aa15bb5a466da3317f1ee2866b462ef640974cb5
Bastian <basbafala@gmail.com>
2020-10-28 14:50:00 +0100

    Test if tileAction changes balance correctly and if til number 10 gives extra turn

---
+++ src/test/BoardTest.java
@@ -0,0 +1,92 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import game.Player;
+import game.Account;
+import game.Board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    Board board = new Board(2, new String[]{"John", "Jane"});
+
+
+    @Test
+    void movePlayer() {
+
+    }
+
+    @Test
+    void tileAction() {
+        boolean xturn = false;
+        for (int i = 0; i < 1000; i++) {
+
+            int money = board.getBalance(0);
+            board.movePlayer(0, 1);
+            if(board.getPosition(0)==10){
+                xturn = true;
+            }
+            else {
+                xturn = false;
+            }
+            assertEquals(xturn, board.getExtraTurn(0));
+            money += getTileAction(board.getPosition(0));
+            board.tileAction(0);
+            assertEquals(money, board.getBalance(0));
+        }
+    }
+    int getTileAction(int number) {
+        switch(number){
+            case 1:
+                return -60;
+                break;
+
+            case 2:
+                return 250;
+                break;
+
+            case 3:
+                return -100;
+                break;
+
+            case 4:
+                return 100;
+                break;
+
+            case 5:
+                return -20;
+                break;
+
+            case 6:
+                return 180;
+                break;
+
+            case 7:
+                return 0;
+                break;
+
+            case 8:
+                return -70;
+                break;
+
+            case 9:
+                return 60;
+                break;
+
+            case 10:
+                return -80;
+                break;
+
+            case 11:
+                return -50;
+                break;
+
+            case 12:
+                return 650;
+                break;
+
+
+        }
+    }
+}
