6ad457ba7c373aa875da5f1ef2cfce45d4cb0c70
Andreas Carlsen <s205437@student.dtu.dk>
2020-10-28 13:20:12 +0100

    Added the BeakerTest class

---
+++ src/test/BeakerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeakerTest {
+
+    @org.junit.jupiter.api.Test
+    void isIdentical() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSum() {
+    }
+}
